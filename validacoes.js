41a2fa1757759d8f8b4f8d71cfb6e5cf7a039ed7
Malvino <149816852+malvino11-28@users.noreply.github.com>
2026-05-25 08:42:56 -0300

    feat: adiciona validacao do CPF do function.js

--- validacoes.js
+++ validacoes.js
@@ -3,6 +3,54 @@ let regex;
 // CPF, CNPJ
 // data futur/passada
 // CEP (API)
+function validarCPF(cpf) {
+
+    if (cpf.length != 11) {
+        return false;
+    }
+    const regex = /^(\d)\1{10}$/; // regex verificando cpf igual (para facilitar)
+    // Impede CPFs iguais
+    if (
+        cpf.test(regex)
+    ) {
+        return false;
+    }
+
+    var soma = 0;
+    var resto;
+
+    for (var i = 1; i <= 9; i++) {
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto == 10 || resto == 11) {
+        resto = 0;
+    }
+
+    if (resto != parseInt(cpf.substring(9, 10))) {
+        return false;
+    }
+
+    soma = 0;
+
+    for (var i = 1; i <= 10; i++) {
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto == 10 || resto == 11) {
+        resto = 0;
+    }
+
+    if (resto != parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
+}
 
 function validarCNPJ(cnpj) {
   let sum = 0,
@@ -75,7 +123,7 @@ function validarData(data) {
 
   dataAtual.setHours(0, 0, 0, 0);
 
-  if (dataForm > dataAtual) return false;
+  if (dataForm > dataAtual) return false; // verificando data futura
   else return true;
 }
 
