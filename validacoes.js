14bf8c843ade532d9617f4449936dd1d61b27017
Malvino30-06 <malvinogarcia2@gmail.com>
2026-05-27 19:48:57 -0300

    feat: adiciona mascaras js

--- validacoes.js
+++ validacoes.js
@@ -317,3 +317,38 @@ if (formCadastro) {
     }
   });
 }
+
+// mascaras
+
+function mascaraCPF(valor) {
+  return valor
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    .slice(0, 14); // slice serve pra garantir que o js vai pegar essa qtd de caracter do valor
+}
+
+function mascaraCNPJ(valor) {
+  return valor
+    .replace(/\D/g, "")
+    .replace(/^(\d{2})(\d)/, "$1.$2")
+    .replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+    .replace(/\.(\d{3})(\d)/, ".$1/$2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .slice(0, 18);
+}
+
+function mascaraTelefone(valor) {
+  return valor
+    .replace(/\D/g, "")
+    .replace(/(\d{5})(\d{1,4})$/, "$1-$2")
+    .slice(0, 10);
+}
+
+function mascaraCEP(valor) {
+  return valor
+    .replace(/\D/g, "")
+    .replace(/^(\d{5})(\d)/, "$1-$2")
+    .slice(0, 9);
+}
